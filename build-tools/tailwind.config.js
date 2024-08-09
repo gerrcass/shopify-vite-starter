b909279d2e4efed5accd8d318a18faca874f0fdc
Gerardo Castillo <gerrcass@gmail.com>
2024-08-09 16:40:41 -0400

    fix: use tailwindcss-rem-to-px to align Tailwind with Dawn theme without disrupting existing styles

--- build-tools/tailwind.config.js
+++ build-tools/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+import remToPxPlugin from 'tailwindcss-rem-to-px';
+
 export default {
   prefix: 'tw-',
   content: ['../**/*.{js,json,liquid}', '!./build-tools/**/*', '!./node_modules/**/*'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    remToPxPlugin({
+      baseFontSize: 16,
+    }),
+  ],
 };
